f6bef6352a9ce25d8aceb7c83cf7eb69c1bfcf6c
Flynn Antonacci <flynn.antonacci@live.com>
2021-09-23 11:27:22 -0600

    button layout, js now increments based on given p

--- Likes/Part 2/script.js
+++ Likes/Part 2/script.js
@@ -1,8 +1,8 @@
-var sim = [0,1,2];
 var element = [document.querySelector('#one'), document.querySelector('#two'), document.querySelector('#three')];
+var sim = [parseInt(element[0].innerHTML), parseInt(element[1].innerHTML), parseInt(element[2].innerHTML)];
 function incrLikes(id) {
   sim[id]++;
-  element[id].innerHTML = sim[id] + " likes";
+  element[id].innerHTML = sim[id] + " like(s)";
 
 }
 
